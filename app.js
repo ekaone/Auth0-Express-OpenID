c7fa27d50f97b3ce536aa90c44900b5a18a7b377
Eka Prasetia <ekaone3033@gmail.com>
2020-11-08 11:41:32 +0700

    add options on auth

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const { auth, requiresAuth } = require("express-openid-connect");
 
-const { auth } = require("express-openid-connect");
 app.use(
   auth({
+    authRequired: false,
+    auth0Logout: true,
     issuerBaseURL: process.env.ISSUER_BASE_URL,
     baseURL: process.env.BASE_URL,
     clientID: process.env.CLIENT_ID,
     secret: process.env.SECRET
   })
 );
+
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
