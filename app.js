d2d208bff9c5675480a08cabf1dee8d234323da7
Eka Prasetia <ekaone3033@gmail.com>
2020-11-08 09:11:20 +0700

    add .env

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
+const { auth } = require("express-openid-connect");
+app.use(
+  auth({
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
